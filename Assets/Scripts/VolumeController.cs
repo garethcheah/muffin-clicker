e22bab3a24dda1e545d08edcd592cd28ca8fbe47
garethcheah <garethcheah@gmail.com>
2024-04-17 10:11:49 -0700

    Adding VolumeController script to VolumeSlider and updating methods in VolumeController

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -3,16 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class VolumeController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
+{  
+    void OnVolumeChanged()
     {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
