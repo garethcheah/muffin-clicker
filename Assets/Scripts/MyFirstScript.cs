9569ad2c75042b802868effeb82832f2a16a928d
garethcheah <garethcheah@gmail.com>
2024-05-08 14:50:13 -0700

    Display number of muffins in scene

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MyFirstScript : MonoBehaviour
 {
+    [SerializeField]
+    private TextMeshProUGUI _totalMuffinsText;
+
     [SerializeField]
     private int _muffinsPerClick = 1;
     private int _counter = 0;
@@ -23,6 +27,7 @@ public class MyFirstScript : MonoBehaviour
     public void OnMuffinClick()
     {
         _counter = _counter + _muffinsPerClick;
+        _totalMuffinsText.text = _counter.ToString();
         Debug.Log(_counter);
     }
 }
