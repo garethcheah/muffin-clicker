9994a455e7d40e6b6ceb2174ce52b3280e068245
garethcheah <garethcheah@gmail.com>
2024-05-13 18:40:05 -0700

    Adding volume functionality to slider

--- Assets/Scripts/VolumeController.cs
+++ Assets/Scripts/VolumeController.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VolumeController : MonoBehaviour
-{  
+{
+    [SerializeField]
+    private Slider _volumeSlider;
+    
     public void OnVolumeChanged()
     {
+        AudioListener.volume = _volumeSlider.value;
         Debug.Log("Volume Changed");
     }
 }
