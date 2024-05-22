c530cc82ed58d1b9d25371d34239ba9e9c24bbf3
garethcheah <garethcheah@gmail.com>
2024-05-22 12:01:55 -0700

    Add changes to totalMuffinsText including creation of new method, concat "muffins" text, and critial hit functionality

--- Assets/Scripts/MyFirstScript.cs
+++ Assets/Scripts/MyFirstScript.cs
@@ -5,16 +5,21 @@ using TMPro;
 
 public class MyFirstScript : MonoBehaviour
 {
+    private const string MUFFIN_TEXT = " muffin";
+    private const string MUFFINS_TEXT = " muffins";
+
     [SerializeField]
     private TextMeshProUGUI _totalMuffinsText;
 
     [SerializeField]
     private int _muffinsPerClick = 1;
+    private int _muffinsCriticalHitMultiplier = 10;
     private int _counter = 0;
 
     // Start is called before the first frame update
     void Start()
     {
+        UpdateTotalMuffins();
         Debug.Log("Hello, World!");
     }
 
@@ -26,8 +31,29 @@ public class MyFirstScript : MonoBehaviour
 
     public void OnMuffinClick()
     {
-        _counter = _counter + _muffinsPerClick;
-        _totalMuffinsText.text = _counter.ToString();
-        Debug.Log(_counter);
+        int randomNumber = Random.Range(1, 100);
+        Debug.Log("randomNumber=" + randomNumber.ToString());
+
+        if (randomNumber != 99)
+        {
+            _counter += _muffinsPerClick;
+        } else
+        {
+            _counter += _muffinsPerClick * _muffinsCriticalHitMultiplier;
+        }
+
+        UpdateTotalMuffins();
+        Debug.Log("counter=" + _counter.ToString());
+    }
+
+    private void UpdateTotalMuffins()
+    {
+        if (_counter == 1)
+        {
+            _totalMuffinsText.text = _counter.ToString() + MUFFIN_TEXT;
+        } else
+        {
+            _totalMuffinsText.text = _counter.ToString() + MUFFINS_TEXT;
+        }
     }
 }
